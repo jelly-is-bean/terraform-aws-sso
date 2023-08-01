aed43125df7dc6dd0aa36b0316362cd40325d9a3
sl-benoitoyez <77098658+sl-benoitoyez@users.noreply.github.com>
2023-08-01 18:41:09 +0200

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -3,7 +3,7 @@ data "aws_ssoadmin_instances" "this" {}
 data "aws_identitystore_group" "this" {
   for_each          = toset(local.groups)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  filter {
+  alternate_identifier {
     attribute_path  = "DisplayName"
     attribute_value = each.value
   }
@@ -12,7 +12,7 @@ data "aws_identitystore_group" "this" {
 data "aws_identitystore_user" "this" {
   for_each          = toset(local.users)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  filter {
+  alternate_identifier {
     attribute_path  = "UserName"
     attribute_value = each.value
   }
